0040a50f73c374945ee7a8ed4983a373c6aad946
abigailfails <thatvaliantvole@gmail.com>
2020-09-22 12:46:14 +0100

    Added more functionality to NumberGuessingGame
    User can now select bound and guess number, and they are told if their guess was below or above the generated number.

--- NumberGuessingGame/src/com/github/abigailfails/numberguessinggame/NumberGuessingGame.java
+++ NumberGuessingGame/src/com/github/abigailfails/numberguessinggame/NumberGuessingGame.java
@@ -6,42 +6,77 @@ import java.util.Random;
 
 public class NumberGuessingGame {
 
-    private static int inputNumber(Scanner inputSource){
+    /**
+     * Use for validating generic integers.
+     **/
+    private static int inputNumber(Scanner inputSource) {
         int inputtedNumber = 0;
-        boolean needsRepeat;
+        boolean needsRepeat = false;
         try {
             inputtedNumber = inputSource.nextInt();
-            needsRepeat = (inputtedNumber<1 || inputtedNumber>10);
         }
         catch(InputMismatchException e){
             needsRepeat = true;
         }
         if(needsRepeat){
-            System.out.println("Please enter a whole number from 1 to 10.");
+            System.out.println("Please enter a whole number.");
             System.out.print("Number: ");
             inputSource.nextLine();
             inputtedNumber = inputNumber(inputSource);
         }
         return inputtedNumber;
     }
-
+    
+    /**
+     * Use for validating guesses.
+     **/
+    private static int inputGuess(Scanner inputSource, int upperBound) {
+        int inputtedNumber = 0;
+        boolean needsRepeat = false;
+        try {
+            inputtedNumber = inputSource.nextInt();
+            needsRepeat = (inputtedNumber<1 || inputtedNumber>upperBound);
+        }
+        catch(InputMismatchException e){
+            needsRepeat = true;
+        }
+        if(needsRepeat){
+            System.out.println("Please enter a whole number from 1 to "+upperBound+".");
+            System.out.print("Number: ");
+            inputSource.nextLine();
+            inputtedNumber = inputGuess(inputSource, upperBound);
+        }
+        return inputtedNumber;
+    }
+    
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        int upperBound;
+        int maxGuesses;
         int inputtedNumber;
         int generatedNumber;
         boolean guessedCorrectly = false;
         boolean shouldContinue = true;
         while(shouldContinue) {
-            generatedNumber = new Random().nextInt(9) + 1;
-            System.out.println("I've chosen a number from 1 to 10. Can you guess what it is?");
-            for(int i=5; i>0; i--) {
+            System.out.println("Please enter the upper bound for the number I'll choose.");
+            System.out.print("Number: ");
+            upperBound = inputNumber(console);
+            System.out.println("Please enter the maximum number of guesses you get.");
+            System.out.print("Number: ");
+            maxGuesses = inputNumber(console);
+            generatedNumber = new Random().nextInt(upperBound-1) + 1;
+            System.out.println("I've chosen a number from 1 to "+upperBound+". Can you guess what it is?");
+            for(int i=maxGuesses; i>0; i--) {
                 System.out.println("You have "+i+" guesses left.");
                 System.out.print("Number: ");
-                inputtedNumber = inputNumber(console);
+                inputtedNumber = inputGuess(console, upperBound);
                 guessedCorrectly = (inputtedNumber == generatedNumber);
                 if(guessedCorrectly){
                     System.out.println("Well done, you guessed correctly! :D");
                     break;
+                }
+                else{
+                    System.out.print(inputtedNumber<generatedNumber ? "You guessed too low. " : "You guessed too high. ");
                 }
             }
             if(!guessedCorrectly) {
@@ -52,7 +87,7 @@ public class NumberGuessingGame {
             String inputtedString = console.next();
             shouldContinue = (inputtedString.toUpperCase()).equals("Y");
         }
-        System.out.println("Thank you for using Times Table Generator!");
+        System.out.println("Thank you for using Number Guessing Software!");
     }
 
 }
