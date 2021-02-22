1176bc2e1add3162529b909f5160962cb995057a
abigailfails <thatvaliantvole@gmail.com>
2021-02-22 12:44:55 +0000

    Minor changes and updated javadocs

--- Graphing/src/hashmap/SimpleHashMap.java
+++ Graphing/src/hashmap/SimpleHashMap.java
@@ -1,8 +1,5 @@
 package hashmap;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,26 +13,43 @@ import java.util.Set;
  * Custom implementation of {@link Map} using a simple hash function of {@code key.hashCode() % size}, permitting null values.
  * <p>Collision-prone and should only be used for small maps.</p>
  *
- * @author dw
+ * @param <K> the type of key to store
+ * @param <V> the type of value to store
  */
-public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
+public class SimpleHashMap<K, V> implements Map<K, V> {
     private SimpleHashMap.Entry<K, V>[] entries;
     private final float loadFactor;
     private int count = 0;
 
+    /**
+     * Creates an empty map with a load factor of 0.75.
+     * */
     public SimpleHashMap() {
         this(0);
     }
 
-    public SimpleHashMap(int startSize) {
-        this(startSize, 0.75f);
+    /**
+     * Creates a map with a specific initial capacity, which helps avoid time-intensive rehashing operations. Has a load
+     * factor of 0.75.
+     *
+     * @param capacity the initial capacity of the map
+     * */
+    public SimpleHashMap(int capacity) {
+        this(capacity, 0.75f);
     }
 
+    /**
+     * Creates a map with a specific initial capacity, which helps avoid time-intensive rehashing operations. Load factor
+     * determined by {@code loadFactor}
+     *
+     * @param capacity the initial capacity of the map
+     * @param loadFactor the load factor the map has to reach before resizing
+     * */
     @SuppressWarnings("unchecked")
-    public SimpleHashMap(int startSize, float loadFactor) {
+    public SimpleHashMap(int capacity, float loadFactor) {
         if (loadFactor <= 0 || loadFactor > 1)
             throw new IllegalArgumentException("Load factor must be between 0 (exclusive) and 1 (inclusive)");
-        this.entries = (Entry<K, V>[]) Array.newInstance(Entry.class, startSize); //fml
+        this.entries = (Entry<K, V>[]) Array.newInstance(Entry.class, capacity); //fml
         this.loadFactor = loadFactor;
     }
 
@@ -49,6 +63,11 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         return key.hashCode() % this.entries.length;
     }
 
+    /**
+     * Private method to change the map's size
+     *
+     * @param newSize the new size of the map
+     * */
     @SuppressWarnings("unchecked")
     private void rehash(int newSize) {
         Entry<K, V>[] oldEntries = this.entries.clone();
@@ -64,14 +83,16 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     }
 
     /**
-     * Finds an index for a {@code int} key. This either finds the position of the key if it already exists, or locates
-     * an empty position to place the key depending on {@code shouldAlreadyContain}.
+     * Finds an index for a key. This either finds the position of the key if it already exists, or locates
+     * an empty position to place the key at if {@code lookForEmpty} is true. If {@code lookForEmpty} is false and no
+     * matching key is found, it returns -1.
+     *
      * <p>On collision this method iterates through the map, looking for a key matching {@code key}, or an empty key </p>
      *
-     * @param key the {@code int} key to find
-     * @param lookForEmpty whether it should be looking for a previously stored value, or for a new location to store.
+     * @param key the key to find the index for
+     * @param lookForEmpty whether empty positions should be count as 'found'
      *
-     * @return the index of {@code key}
+     * @return the index of {@code key} or an empty space, or -1 if no key exists and {@code lookForEmpty} is false
      * */
     private int findIndex(Object key, boolean lookForEmpty) {
         int start = hash(key);
@@ -90,12 +111,12 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     }
 
     /**
-     * Attempts to add a new key-value pair to the map.
+     * Attempts to add a new key-value pair to the map, or change the value of a pre-existing mapping.
      *
      * @param key the {@code int} key to add
      * @param value {@link V} the value to add
      *
-     * @return the previous value stored with {@code key}, or {@code null if none existed}.
+     * @return the previous value stored with {@code key}, or {@code null} if none existed
      * */
     @Override
     public V put(K key, V value) {
@@ -111,11 +132,11 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     }
 
     /**
-     * Attempts to find the value for an {@code int} key
+     * Attempts to find the value stored for a key
      *
      * @param key the {@code int} key to search for
      *
-     * @return the {@link V} value corresponding to {@code key}
+     * @return the {@link V} value corresponding to {@code key}, or {@code null} if none exists
      * */
     @Override
     public V get(Object key) throws IllegalArgumentException {
@@ -124,26 +145,34 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     }
 
     /**
-     * Attempts to remove a key-value pair from the map given an {@code int} key to remove
+     * Attempts to remove a key-value pair from the map given a key to remove
      *
      * @param key the {@code int} key to remove
      *
-     * @return the value that was removed, or {@code null} if no key matching {@code key} existed.
+     * @return the value that was removed, or {@code null} if no key matching {@code key} existed
      * */
     @Override
     public V remove(Object key) throws IllegalArgumentException {
         int index = findIndex(key, false);
         if (index > -1) {
             Entry<K, V> entry = entries[index];
+            V value = entry.getValue();
             entry.key = new Object();
             if ((this.entries.length) - (this.size() + 1) > entries.length - ((int) (entries.length * loadFactor)))
                 this.rehash(this.entries.length - 1);
             this.count--;
-            return entry.getValue();
+            return value;
         }
         return null;
     }
 
+    /**
+     * Puts all of the mappings in another map into this map
+     *
+     * @param map the map to pull mappings from
+     *
+     * @see SimpleHashMap#put(K, V)
+     * */
     @Override
     public void putAll(Map<? extends K, ? extends V> map) {
         for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
@@ -151,12 +180,20 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         }
     }
 
+    /**
+     * Removes all the mappings from the map.
+     * */
     @Override
     public void clear() {
         Arrays.fill(this.entries, null);
         this.count = 0;
     }
 
+    /**
+     * Creates a set of all the keys in the map.
+     *
+     * @return a set of the map's keys
+     * */
     @Override
     public Set<K> keySet() {
         HashSet<K> set = new HashSet<>();
@@ -167,6 +204,11 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         return set;
     }
 
+    /**
+     * Creates a collection of all the values for the map's keys.
+     *
+     * @return a collections of the map's values
+     * */
     @Override
     public Collection<V> values() {
         Collection<V> collection = new ArrayList<>();
@@ -177,6 +219,11 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         return collection;
     }
 
+    /**
+     * Creates a set of the map's {@link Map.Entry} instances, containing both their keys and their values.
+     *
+     * @return a set of the map's entries
+     * */
     @Override
     public Set<Map.Entry<K, V>> entrySet() {
         HashSet<Map.Entry<K, V>> set = new HashSet<>();
@@ -188,7 +235,7 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     }
 
     /**
-     * Returns how many key-value pairs are in the map, not including empty values
+     * Returns how many key-value pairs are in the map, not including empty spaces in the internal array
      *
      * @return the number of items in the map
      * */
@@ -210,7 +257,7 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     /**
      * Returns whether the map contains a key-value pair matching {@code key}
      *
-     * @param key the {@code Object} key to search for
+     * @param key the key to search for
      * @return whether the list contains {@code key}
      * */
     @Override
@@ -222,7 +269,7 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
     /**
      * Returns whether the map contains one or more keys that map to {@code value}
      *
-     * @param value the {@code Object} value to search for
+     * @param value the value to search for
      * @return whether the list contains {@code value}
      * */
     @Override
@@ -233,15 +280,31 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         return false;
     }
 
+    /**
+     * Implementation of {@link Map.Entry} that allows keys that aren't instances of {@code V} in order to indicate that
+     * a key was deleted so that search operations still work.
+     * */
     static class Entry<K, V> implements Map.Entry<K, V> {
         protected Object key; //Object so it can hold deletion markers
         protected V value;
 
+        /**
+         * Creates a new {@link Entry} with a specified key and value
+         *
+         * @param key the key to store
+         * @param value the value to store
+         * */
         public Entry(K key, V value) {
             this.key = key;
             this.value = value;
         }
 
+        /**
+         * Returns the stored key, or throws an exception if it's been deleted.
+         *
+         * @return the key that was stored
+         * @throws IllegalStateException if the stored key isn't an instance of {@link K}
+         * */
         @SuppressWarnings("unchecked")
         @Override
         public K getKey() {
@@ -250,11 +313,21 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
             return (K) this.key;
         }
 
+        /**
+         * Returns the stored value.
+         *
+         * @return the value that was stored
+         * */
         @Override
         public V getValue() {
             return this.value;
         }
 
+        /**
+         * Changes the stored value for the entry.
+         *
+         * @return the previously-stored value
+         * */
         @Override
         public V setValue(V value) {
             V old = this.value;
@@ -281,6 +354,7 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
         }
     }
 
+    //Might collide too much
     @Override
     public int hashCode() {
         int code = 0;
@@ -288,15 +362,5 @@ public class SimpleHashMap<K, V> implements Map<K, V>, Serializable {
             code ^= entry == null ? 0 : entry.hashCode();
         }
         return code;
-    }
-
-    private void writeObject(ObjectOutputStream out) {
-
-    }
-    private void readObject(ObjectInputStream in) {
-
-    }
-    private void readObjectNoData() {
-
     }
 }
